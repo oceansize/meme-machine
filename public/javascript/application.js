095a2e5d683f9fc66e772cc8090b51f9880f5e94
Roi Driscoll <roi.driscoll@gmail.com>
2014-03-08 15:02:04 +0000

    second version

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,11 @@
 $(document).on('click', '#go-search', function() {
   findImagesOnGoogle({keywords: $('#search-term').val(), container: '#search-results'}) 
 }); 
+
+$(document).on('click', '#search-results img', function()
+  {
+    var url = $(this).data('url');
+    $("#workspace img").remove();
+    var img = $("<img>").attr('src', url);
+    $("#workspace").append(img);
+  });
