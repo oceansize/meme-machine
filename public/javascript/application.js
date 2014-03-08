01c250d26f4e087c8522711fcc41dfc4b4202412
Roi Driscoll <roi.driscoll@gmail.com>
2014-03-08 23:26:58 +0000

    fourth version

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,9 @@
+// Performs a Google image search based on text in #search-term, and places the results in #search-results.
 $(document).on('click', '#go-search', function() {
   findImagesOnGoogle({keywords: $('#search-term').val(), container: '#search-results'}) 
 }); 
 
+//When an image result is clicked, the url is stored in 'url'. Then #workspace is cleared of any existing images. A new img element is then created and the image result in 'url' is loaded into it.
 $(document).on('click', '#search-results img', function() {
     var url = $(this).data('url');
     $("#workspace img").remove();
@@ -9,6 +11,34 @@ $(document).on('click', '#search-results img', function() {
     $("#workspace").append(img);
   });
 
+//When text is input into #text, the value is output into #caption.
 $(document).on('input', '#text', function() {
     $("#caption").text($(this).val());
 });
+
+//When the amount in the 'left' input changes, apply to #caption the value in pixels to the css left attribute. 
+$(document).on('change', '#left' , function() {
+    $("#caption").css("left", $(this).val() + 'px');
+});
+//Same as above, but for top
+$(document).on('change', '#top', function() {
+  $("#caption").css("top", $(this).val() + 'px');
+});
+//This alters the width of #caption
+$(document).on('change', '#width', function() {
+  $('#caption').css('width', $(this).val() + 'px');
+});
+//and the  text size in  #caption
+$(document).on('change', '#size', function() {
+  $('#caption').css('font-size', $(this).val() + 'px');
+});
+//This chances the colour of #caption
+$(document).on('change', '#colour', function() {
+  $('#caption').css('color', $(this).val() );
+});
+//and this alters the alignment.
+$(document).on('change', '#alignment', function() {
+  $('#caption').css('text-align', $(this).val() );
+});
+
+
